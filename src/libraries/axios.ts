9f9b38331cf325bb4987cfc9b75fab28f2da92b1
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-03 11:45:44 -0300

    feat: add config axios

---
+++ src/libraries/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const poketeamApi = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+export const pokeApi = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/pokemon'
+})
