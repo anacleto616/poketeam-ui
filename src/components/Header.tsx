f7a267f914fdfe43406b676f183924cb4d0a9c3a
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-03 11:46:38 -0300

    feat: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-gray-300 h-16 p-12 w-full flex items-center justify-around max-md:flex-col">
+      <span className="font-medium text-black">Home</span>
+      <span className="font-medium text-black">Times Pokemon</span>
+      <span className="font-medium text-black">Cadastrar Times Pokemon</span>
+    </div>
+  );
+};
+
+export default Header;
